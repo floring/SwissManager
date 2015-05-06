31d0bf9cf5626fbd30e152751d293c1bce8623a2
floring <floring@users.noreply.github.com>
2015-05-06 19:58:40 +0300

    Add toolbar layout, application style theme

---
+++ app/src/main/java/com/arles/swissmanager/ui/fragment/NavigationDrawerFragment.java
@@ -0,0 +1,13 @@
+package com.arles.swissmanager.ui.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Admin on 06.05.2015.
+ */
+public class NavigationDrawerFragment extends Fragment {
+
+    public NavigationDrawerFragment() {
+        // Required empty public constructor
+    }
+}
