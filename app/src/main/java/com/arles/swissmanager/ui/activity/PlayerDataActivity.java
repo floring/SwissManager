bdff5d6c45a0220e651f2c042b27f8f9ed58e57f
floring <floring@users.noreply.github.com>
2015-09-02 13:00:17 +0300

    Remove menu from activities: NewPlayer, PlayerData.

--- app/src/main/java/com/arles/swissmanager/ui/activity/PlayerDataActivity.java
+++ app/src/main/java/com/arles/swissmanager/ui/activity/PlayerDataActivity.java
@@ -55,24 +55,6 @@ public class PlayerDataActivity extends BaseActivity implements PlayerDataPresen
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_player_data, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public void setViewComponent() {
         setRecyclerView();
         setViewFieldValue();
